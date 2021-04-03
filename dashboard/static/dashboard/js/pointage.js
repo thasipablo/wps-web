05d1866f87efb5524bc35c1ed3dea01fb9bb5862
nkayisi <nelsonkayisirirya5@gmail.com>
2021-04-03 17:09:19 +0200

    Sign the attendance even if the student is not yet enrolled

--- dashboard/static/dashboard/js/pointage.js
+++ dashboard/static/dashboard/js/pointage.js
@@ -14,8 +14,8 @@ window.addEventListener('DOMContentLoaded', (e) => {
             .then(data => data)
             .catch(err => console.error(err))
 
+        // Sign Attendance
         const signAttendance = (rfId) => {
-
             let data = {rf_id: rfId}
 
             fetch('attendance/api/attendances', {
@@ -32,7 +32,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
         }
 
         /*
-         * POINTAGE
+         * POINTAGE PROCESS
          */
         rfIdField.addEventListener('keyup', (event) => {
             // on enter key up
@@ -56,16 +56,21 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
                 } else {
                     studentNameTag.innerHTML = `<div style="color:red;">VOUS N'ETES PAS ENCORE ENREGISTRE</div>`
-                    rfIdField.value = ''
                     checkImgTag.innerHTML = `
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
                         </svg>
                     `
+                    // then record the presence
+                    signAttendance(rfIdField.value)
+
+                    rfIdField.value = ''
                     checkImgTag.classList.remove('success')
                     checkImgTag.classList.add('error')
                 }
+
             }
+            
         })
         
     }()
